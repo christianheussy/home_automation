89dedac11faeff2cb7e22787ef31614160bc0fa1
christianheussy <heussy@gmail.com>
2018-11-15 09:10:00 -0800

    Added start and run methods for threading.

--- utils/sensor.py
+++ utils/sensor.py
@@ -8,11 +8,25 @@ class Sensor:
         
     def get_temp(self):
         humidity, temp = Adafruit_DHT.read_retry(self.sensor, self.pin)
-        return temp* 9/5.0 +32
+        return temp* 9/5.0 +32, humidity
     
-    def get_humidity(self):
-        humidity, temp = Adafruit_DHT.read_retry(self.sensor, self.pin)
-        return humidity
+    def start(self, daemon=True, period=1):
+        """This method starts a thread"""
+        t = threading.Thread(target=self.run, args=[1])
+        if daemon:
+            t.daemon = True
+        t.start()
 
-    
-    
+    def run(self, period):
+        """This method periodically polls the sensor and writes to a DB"""
+        while True:
+            #temp, humidity = self.get_temp()
+            print('Working')
+
+            time.sleep(period)
+
+
+if __name__ == '__main__':
+    sensor = Sensor()
+    sensor.start()
+    time.sleep(5)
